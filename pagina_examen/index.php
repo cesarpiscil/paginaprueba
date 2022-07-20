d1ec9b16fd11e3656adf2d98f007ca632a36379d
cesarpiscil <cesarpiscil68@gmail.com>
2022-07-20 02:22:19 -0500

    Se agrega el proyecto

---
+++ pagina_examen/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>examen parcial</title>
+</head>
+<body>
+
+    <?php 
+        $data = json_decode(file_get_contents("https://jsonplaceholder.typicode.com/todos"), true);    
+    ?>
+    
+</body>
+</html>
